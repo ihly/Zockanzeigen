45b56f778c22f9b671cbf02e1b3837942238ced8
Michael IHly <michi.ihly@gmail.com>
2018-10-24 16:51:26 +0200

    unnötige dateien gelöscht

--- image_upload.sql
+++ image_upload.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 24. Okt 2018 um 14:22
+-- Erstellungszeit: 24. Okt 2018 um 16:50
 -- Server-Version: 10.1.36-MariaDB
 -- PHP-Version: 7.2.10
 
@@ -32,22 +32,27 @@ CREATE TABLE `images` (
   `id` int(11) NOT NULL,
   `image` varchar(100) NOT NULL,
   `image_text` text NOT NULL,
-  `price` float NOT NULL,
-  `category` varchar(20) NOT NULL
+  `price` float DEFAULT NULL,
+  `category` varchar(20) NOT NULL,
+  `name` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `images`
 --
 
-INSERT INTO `images` (`id`, `image`, `image_text`, `price`, `category`) VALUES
-(35, 'unnamed.jpg', 'dfdfd', 1000000, 'Nintendo Spiel'),
-(36, 'unnamed.jpg', 'dfdfdf', 2000000, 'PS4 Spiel'),
-(37, 'lambo.jpg', 'test', 2000000, 'Nintendo Spiel'),
-(38, 'unnamed.jpg', 'fdf', 10000, 'Konsole'),
-(39, 'unnamed.jpg', 'vdfdf', 10003, 'PS4 Spiel'),
-(40, 'unnamed.jpg', '		fvfgf', 30.56, 'PS4 Spiel'),
-(41, 'unnamed.jpg', '	187	', 30.56, 'X-BOX Spiel');
+INSERT INTO `images` (`id`, `image`, `image_text`, `price`, `category`, `name`) VALUES
+(35, 'unnamed.jpg', 'dfdfd', 1000000, 'Nintendo Spiel', ''),
+(36, 'unnamed.jpg', 'dfdfdf', 2000000, 'PS4 Spiel', ''),
+(37, 'lambo.jpg', 'test', 2000000, 'Nintendo Spiel', ''),
+(38, 'unnamed.jpg', 'fdf', 10000, 'Konsole', ''),
+(39, 'unnamed.jpg', 'vdfdf', 10003, 'PS4 Spiel', ''),
+(40, 'unnamed.jpg', '		fvfgf', 30.56, 'PS4 Spiel', ''),
+(41, 'unnamed.jpg', '	187	', 30.56, 'X-BOX Spiel', ''),
+(43, 'lambo.jpg', '		neu', 25, 'PS4 Spiel', 'gta 5'),
+(47, 'unnamed.jpg', '	dfd	', 34, 'Konsole', 'ihlyi'),
+(48, 'unnamed.jpg', '	df	', 0, 'PS4 Spiel', 'efdfdfd'),
+(54, 'unnamed.jpg', '	df	', 5555560, 'Konsole', 'dfdfd');
 
 --
 -- Indizes der exportierten Tabellen
@@ -67,7 +72,7 @@ ALTER TABLE `images`
 -- AUTO_INCREMENT für Tabelle `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
